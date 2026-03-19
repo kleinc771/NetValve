07bc663f4e59b391ccf10c59a850be019fa0d16e
kleinc771 <kleinc771@gmail.com>
2026-03-19 08:17:55 +0530

    Update MainActivity.kt

--- app/src/main/java/com/netvalve/app/MainActivity.kt
+++ app/src/main/java/com/netvalve/app/MainActivity.kt
@@ -1,33 +1,20 @@
 package com.netvalve.app
 
 import android.os.Bundle
+import android.widget.TextView
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-import android.content.Intent
-import android.net.VpnService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val btn = Button(this)
-        btn.text = "Start NetValve"
-        setContentView(btn)
         
-        btn.setOnClickListener {
-            val intent = VpnService.prepare(this)
-            if (intent != null) {
-                startActivityForResult(intent, 0)
-            } else {
-                onActivityResult(0, RESULT_OK, null)
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK) {
-            val intent = Intent(this, NetValveVpnService::class.java)
-            startService(intent)
-        }
+        // This creates a simple screen so we can see the app is alive!
+        val textView = TextView(this)
+        textView.text = "NetValve is Running!"
+        textView.textSize = 24f
+        textView.gravity = Gravity.CENTER
+        
+        setContentView(textView)
     }
 }
